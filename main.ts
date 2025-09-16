aeed9d46cde3eb32ba094c8f56e48a693345d57e
by0115115-cell <by0115115@gmail.com>
2025-09-15 22:51:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+radio.sendNumber(15)
+basic.forever(function () {
+    music.rest(10 + 5)
+    while (true) {
+        continue;
+    }
+    break;
+})
